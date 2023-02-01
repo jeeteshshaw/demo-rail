8cc6a3feaafed78f8970680eac70ba07a1890790
Jeetesh Kumar Shaw <jeeteshshaw@gmail.com>
2023-02-02 00:26:08 +0530

    train form done

--- navigation/BottomTab.tsx
+++ navigation/BottomTab.tsx
@@ -1,25 +1,31 @@
-import { Easing, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Easing, StyleSheet, Text, View, TouchableHighlight } from 'react-native'
+import React, { useState } from 'react'
 import { createMaterialBottomTabNavigator, MaterialBottomTabScreenProps } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home, { SettingsScreen } from '../Screens/Home/Home';
+// import { useTheme } from 'react-native-paper';
+import {  useTheme } from '@react-navigation/native';
 
 const Tab = createMaterialBottomTabNavigator();
 
 const BottomTab = () => {
+  // useState
+  const {colors} = useTheme()
   return (
     <Tab.Navigator
     initialRouteName="Feed"
-    activeColor="#F67325"
+    
+    activeColor={"#F67325"}
     shifting={true}
     // sceneAnimationType='opacity'
     // sceneAnimationEnabled={true}
     // sceneAnimationEasing={Easing.bounce}
-    inactiveColor="#000"
+    inactiveColor="#cccccc"
+   
     screenOptions={{
       // tabBarBadge:10,
-      tabBarLabel:"#fff",
-      tabBarColor:"#fff",
+      tabBarLabel:"#F67325",
+      tabBarColor:"#F67325",
       
     }}
     style={{
@@ -27,23 +33,32 @@ const BottomTab = () => {
       // borderTopRightRadius:20
       // paddingTop:20, 
     }}
-    barStyle={{ backgroundColor: '#0275d8', }}
+    labeled={true}
+    sceneAnimationEasing={Easing.linear}
+    // screenListeners={{focus:({target})=>console.log(target)}}
+    barStyle={{ backgroundColor: "#fff", borderTopWidth:1, borderColor:"#F67325", height:70  }}
   >
     <Tab.Screen
       name="Feed"
       component={Home}
+      
       options={{
+        
         tabBarLabel: 'Home',
         tabBarIcon: ({ color }) => (
           <MaterialCommunityIcons name="home" color={color} size={26} />
         ),
+        
       }}
+      
     />
     <Tab.Screen
       name="Notifications"
-      component={Home}
+      component={SettingsScreen}
+      
       options={{
-        tabBarLabel: 'Updates',
+        tabBarLabel: 'Notifications',
+        tabBarBadge:10,
         tabBarIcon: ({ color }) => (
           <MaterialCommunityIcons name="bell" color={color} size={26} />
         ),
@@ -51,11 +66,21 @@ const BottomTab = () => {
     />
     <Tab.Screen
       name="Profile"
-      component={Home}
+      component={SettingsScreen}
       options={{
         tabBarLabel: 'Profile',
         tabBarIcon: ({ color }) => (
           <MaterialCommunityIcons name="account" color={color} size={26} />
+        ),
+      }}
+    />
+     <Tab.Screen
+      name="Setting"
+      component={SettingsScreen}
+      options={{
+        tabBarLabel: 'Setting',
+        tabBarIcon: ({ color }) => (
+          <MaterialCommunityIcons name="cog" color={color} size={26} />
         ),
       }}
     />
